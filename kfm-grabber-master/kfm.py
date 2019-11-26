dccb32027dc0685049121ad9fb127135b43145fb
User <tvd1604@gmail.com>
2019-11-26 17:07:39 +0600

    Hotfix
    Hotfix

--- kfm-grabber-master/kfm.py
+++ kfm-grabber-master/kfm.py
@@ -3,7 +3,7 @@ from app.xmlParser import Parser
 from app.creds import AppSetting
 from app.db_queries import Kfm
 import logging
-from app.mailer import wooppay_smtp
+# from app.mailer import wooppay_smtp
 
 logging.basicConfig(
     format=u'%(levelname)-8s [%(asctime)s] | %(message)s',
@@ -26,7 +26,8 @@ class Monitor(object):
         arr_persons = Parser(xmldoc=xml).get_persons()
         arr_organisations = Parser(xmldoc=xml).get_organisations()
         arr_organisationscis = Parser(xmldoc=xml).get_organisationscis()
-        arr_ogranisationsus = Parser(xmldoc=xml).get_ogranisationsun()
+        arr_un_individuals = Parser(xmldoc=None).get_un_individuals()
+        arr_un_entities = Parser(xmldoc=None).get_un_entities()
 
         for person in arr_persons:
             sess.add_person(person)
@@ -34,8 +35,10 @@ class Monitor(object):
             sess.add_organisation(org)
         for org_cis in arr_organisationscis:
             sess.add_organisationcis(org_cis)
-        for org_un in arr_ogranisationsus:
-            sess.add_ogranisationus(org_un)
+        for individual in arr_un_individuals:
+            sess.add_un_individuals(individual)
+        for entity in arr_un_entities:
+            sess.add_un_entities(entity)
 
     def update_excluded(self, sess, kfm_data):
         """
@@ -49,7 +52,8 @@ class Monitor(object):
         arr_persons = Parser(xmldoc=xml).get_persons()
         arr_organisations = Parser(xmldoc=xml).get_organisations()
         arr_organisationscis = Parser(xmldoc=xml).get_organisationscis()
-        arr_ogranisationsun = Parser(xmldoc=xml).get_ogranisationsun()
+        arr_un_individuals = Parser(xmldoc=None).get_un_individuals()
+        arr_un_entities = Parser(xmldoc=None).get_un_entities()
         for person in arr_persons:
             if not person.get('correction'):
                 sess.del_person(person)
@@ -57,9 +61,10 @@ class Monitor(object):
             sess.del_organisation(org)
         for org_cis in arr_organisationscis:
             sess.del_all_organisationscis(org_cis)
-        for org_un in arr_ogranisationsun:
-            sess.del_all_organisationsun(org_un)
-
+        for individual in arr_un_individuals:
+            sess.del_all_un_individuals(individual)
+        for entity in arr_un_entities:
+            sess.del_all_un_entities(entity)
 
     def start(self):
         kfm_data = KfmWorker()
@@ -73,12 +78,16 @@ class Monitor(object):
             db_sess.update_relevance('organisations')
             self.update_excluded(db_sess, kfm_data.get_excluded())
             db_sess.update_relevance('organisationscis')
+            self.update(db_sess, kfm_data.get_consolidated())
+            db_sess.update_relevance('un_individuals')
+            self.update(db_sess, kfm_data.get_consolidated())
+            db_sess.update_relevance('un_entities')
             db_sess.session_close()
         except Exception as e:
             db_sess.session_rollback()
             db_sess.session_close()
             logging.exception(e)
-            wooppay_smtp('kfm-grabber', str(e), AppSetting.trace_mail)
+            # wooppay_smtp('kfm-grabber', str(e), AppSetting.trace_mail)
 
 
 if __name__ == '__main__':
